c7284cd512e9f0b7d783264f732f016d661bcbc7
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2023-12-05 10:15:35 -0500

    1

--- api/main.js
+++ api/main.js
@@ -1,12 +1,29 @@
 const url = "https://eldenring.fanapis.com/api/bosses?limit=170";
 const dom = {
   display: document.querySelector(".display"),
+  displayCard: document.querySelector("#card"),
 };
 
 async function getdata(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    return data.data;
   } catch (error) {}
 }
+const promise = getdata(url);
+
+promise.then(function (boss) {
+  console.log(boss);
+  function insertCard(boss) {
+    return `<div class="card">
+      <img src=${boss.image}.image}>
+      <h2>${boss.name}</h2>
+    </div>;`;
+  }
+  insertCard(boss);
+});
+
+boss.forEach((boss) => {
+  dom.display.insertAdjacentHTML("afterbegin", insertCard(boss));
+});
