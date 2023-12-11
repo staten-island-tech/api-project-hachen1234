0c0d4e6e2d5b8ecd8018fa68f61471a6f0ba8bfa
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2023-12-11 10:15:42 -0500

    1

--- api/main.js
+++ api/main.js
@@ -3,12 +3,14 @@ const url2 = "https://eldenring.fanapis.com/api/sorceries?limit=1000";
 const dom = {
   display: document.querySelector(".display"),
   displayCard: document.querySelector("#card"),
+  bossesbtn: document.querySelector(".bosses"),
 };
 
 async function getdata(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
+    console.log(data);
     console.log(data.data);
     return data.data;
   } catch (error) {
@@ -16,19 +18,25 @@ async function getdata(url) {
   }
 }
 
-const promise = getdata(url);
-
-promise.then(function insertcard(data) {
-  data.forEach((data) => {
-    dom.display.insertAdjacentHTML(
-      "beforeend",
-      ` 
-      <div class="card">
-            <h2 class="name">${data.name}</h2>
-            <img class="image"src="${data.image}" alt="">
-            <h3 class="text">${data.description}</h3>
-            <p class="text">${data.location}</p>
-            `
-    );
+dom.bossesbtn.addEventListener("click", function () {
+  let promise = getdata(url);
+  promise.then(function insertcard(data) {
+    data.forEach((data) => {
+      dom.display.insertAdjacentHTML(
+        "beforeend",
+        ` 
+        <div class="card">
+        <p class="subtitle">Name</p>
+              <h3 class="name">${data.name}</h3>
+              <img class="image"src="${data.image}" alt="">
+              <p class="subtitle">Description</p>
+              <h3 class="description">${data.description}</h3>
+              <p class="subtitle">Location</p>
+              <h3 class="text">${data.location}</h3>
+              <p class="subtitle">Drops</p>
+              <h3 class="drops">${data.drops}</h3>
+              `
+      );
+    });
   });
 });
