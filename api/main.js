d04f1f605f46ba10db933f549dd0d3c53cbb6438
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2023-12-12 09:50:28 -0500

    1

--- api/main.js
+++ api/main.js
@@ -1,25 +1,27 @@
-const url = "https://eldenring.fanapis.com/api/bosses?limit=1000";
-const url2 = "https://eldenring.fanapis.com/api/sorceries?limit=1000";
+const ur1 = "https://eldenring.fanapis.com/api?limit=1000";
+const url2 = "https://eldenring.fanapis.com/api/bosses?limit=1000";
+const url3 = "https://eldenring.fanapis.com/api/sorceries?limit=1000";
 const dom = {
   display: document.querySelector(".display"),
   displayCard: document.querySelector("#card"),
   bossesbtn: document.querySelector(".bosses"),
+  sorceriesbtn: document.querySelector(".sorceries"),
 };
-
-async function getdata(url) {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    console.log(data.data);
-    return data.data;
-  } catch (error) {
-    document.querySelector("h1").textContent = "sad";
-  }
+function clear() {
+  dom.display.innerHTML = "";
 }
-
 dom.bossesbtn.addEventListener("click", function () {
-  let promise = getdata(url);
+  clear();
+  async function getdata(url2) {
+    try {
+      const response = await fetch(url2);
+      const data = await response.json();
+      return data.data;
+    } catch (error) {
+      document.querySelector("h1").textContent = "sad";
+    }
+  }
+  let promise = getdata(url2);
   promise.then(function insertcard(data) {
     data.forEach((data) => {
       dom.display.insertAdjacentHTML(
@@ -35,6 +37,38 @@ dom.bossesbtn.addEventListener("click", function () {
               <h3 class="text">${data.location}</h3>
               <p class="subtitle">Drops</p>
               <h3 class="drops">${data.drops}</h3>
+              `
+      );
+    });
+  });
+});
+dom.sorceriesbtn.addEventListener("click", function () {
+  clear();
+  async function getdata(url3) {
+    try {
+      const response = await fetch(url3);
+      const data = await response.json();
+      return data.data;
+    } catch (error) {
+      document.querySelector("h1").textContent = "sad";
+    }
+  }
+  let promise = getdata(url3);
+  promise.then(function insertcard(data) {
+    data.forEach((data) => {
+      dom.display.insertAdjacentHTML(
+        "beforeend",
+        ` 
+        <div class="card">
+        <p class="subtitle">Name</p>
+              <h3 class="name">${data.name}</h3>
+              <img class="image"src="${data.image}" alt="">
+              <p class="subtitle">Description</p>
+              <h3 class="description">${data.description}</h3>
+              <p class="subtitle">Effects</p>
+              <h3 class="text">${data.effects}</h3>
+              <p class="subtitle">Drops</p>
+              <h3 class="cost">${data.cost}</h3>
               `
       );
     });
