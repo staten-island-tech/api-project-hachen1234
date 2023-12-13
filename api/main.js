a13abdd8f67c6fa3d0dfba9d1b36633cdbd79af0
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2023-12-13 10:16:22 -0500

    .

--- api/main.js
+++ api/main.js
@@ -1,14 +1,47 @@
+const url = "	https://eldenring.fanapis.com/api/npcs?limit=1000";
 const url2 = "https://eldenring.fanapis.com/api/bosses?limit=1000";
 const url3 = "https://eldenring.fanapis.com/api/sorceries?limit=1000";
 const dom = {
   display: document.querySelector(".display"),
   displayCard: document.querySelector("#card"),
+  npcbtn: document.querySelector(".npc"),
   bossesbtn: document.querySelector(".bosses"),
   sorceriesbtn: document.querySelector(".sorceries"),
 };
 function clear() {
   dom.display.innerHTML = "";
 }
+dom.npcbtn.addEventListener("click", function () {
+  clear();
+  async function getdata(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data.data;
+    } catch (error) {
+      document.querySelector("h1").textContent = "sad";
+    }
+  }
+  let promise = getdata(url);
+  promise.then(function insertcard(data) {
+    data.forEach((data) => {
+      dom.display.insertAdjacentHTML(
+        "beforeend",
+        ` <div>
+        <div class="card">
+        <p class="subtitle">Name</p>
+              <h3 class="name">${data.name}</h3>
+              <img class="image"src="${data.image}" alt="">
+              <p class="subtitle">Quote</p>
+              <h3 class="description">${data.quote}</h3>
+              <p class="subtitle">Location</p>
+              <h3 class="text">${data.location}</h3>
+              <div>
+              `
+      );
+    });
+  });
+});
 dom.bossesbtn.addEventListener("click", function () {
   clear();
   async function getdata(url2) {
