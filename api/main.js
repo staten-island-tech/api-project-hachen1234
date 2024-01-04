a0c5b757de2c78a408f9ef7f088ffed617985b36
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2024-01-04 09:34:47 -0500

    Revert "1"
    This reverts commit 076610d2a28e7f84451f31668cfcc5021eea2121.

--- api/main.js
+++ api/main.js
@@ -1,6 +1,34 @@
-async function getdata() {
-  let res = await fetch("https://eldenring.fanapis.com/api/bosses?limit=1000");
-  let data = await res.json();
-  console.log(data);
+const url = "https://eldenring.fanapis.com/api/bosses?limit=1000";
+const url2 = "https://eldenring.fanapis.com/api/sorceries?limit=1000";
+const dom = {
+  display: document.querySelector(".display"),
+  displayCard: document.querySelector("#card"),
+};
+
+async function getdata(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.data);
+    return data.data;
+  } catch (error) {
+    document.querySelector("h1").textContent = "sad";
+  }
 }
-getdata(data);
+
+const promise = getdata(url);
+
+promise.then(function insertcard(data) {
+  data.forEach((data) => {
+    dom.display.insertAdjacentHTML(
+      "beforeend",
+      ` 
+      <div class="card">
+            <h2 class="name">${data.name}</h2>
+            <img class="image"src="${data.image}" alt="">
+            <h3 class="text">${data.description}</h3>
+            <p class="text">${data.location}</p>
+            `
+    );
+  });
+});
